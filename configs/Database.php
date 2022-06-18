1680afb7e283d92fd9d33171602bcf9c90359cfb
Trần Hà <92640384+tranha2511@users.noreply.github.com>
2022-06-18 13:06:30 +0700

    Update Database.php

--- configs/Database.php
+++ configs/Database.php
@@ -2,12 +2,12 @@
 
 class Database
 {
-  //const DB_DSN = 'mysql:host=localhost;dbname=PHP0721E_nhom3;charset=utf8';
-  //const DB_USERNAME = 'root';
- // const DB_PASSWORD = '';
+  const DB_DSN = 'mysql:host=localhost;dbname=PHP0721E_nhom3;charset=utf8';
+  const DB_USERNAME = 'root';
+  const DB_PASSWORD = '';
 
-   const DB_DSN = 'mysql:host=localhost;dbname=PHP0721E_nhom3;charset=utf8';
-   const DB_USERNAME = 'PHP0721E_nhom3';
-   const DB_PASSWORD = '123456';
+  // const DB_DSN = 'mysql:host=localhost;dbname=PHP0721E_nhom3;charset=utf8';
+  // const DB_USERNAME = 'PHP0721E_nhom3';
+   //const DB_PASSWORD = '123456';
   
 }
